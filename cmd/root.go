68ca84c8f6d5c00dbed268d720da44585356aada
Olly Britton <ollybritton@gmail.com>
2020-07-09 15:31:45 +0100

    share work

--- cmd/root.go
+++ cmd/root.go
@@ -3,11 +3,12 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/ollybritton/cal8/pkg/cal8"
 	"github.com/spf13/cobra"
-
-	homedir "github.com/mitchellh/go-homedir"
-	"github.com/spf13/viper"
+	"github.com/tj/go-naturaldate"
 )
 
 var cfgFile string
@@ -15,16 +16,41 @@ var cfgFile string
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "cal8",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "cal8 is an alertnative calendar utility.",
+	Long: `cal8 is an experiment in a different calendar system.
+	
+Under the cal8 system, weeks are 8 days long and consist of the typical Monday
+to Friday, plus an extra "Valday".`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
+	Run: func(cmd *cobra.Command, args []string) {
+		cal := getCal(cmd)
+
+		compare, err := cmd.PersistentFlags().GetBool("compare")
+		if err != nil {
+			fmt.Println("error parsing compare flag:", err)
+			os.Exit(1)
+		}
+
+		var date time.Time
+
+		if len(args) == 0 {
+			date = time.Now()
+		} else {
+			date, err = naturaldate.Parse(
+				strings.Join(args, " "),
+				time.Now(),
+				naturaldate.WithDirection(naturaldate.Past),
+			)
+
+			if err != nil {
+				fmt.Println("error parsing date specified:", err)
+			}
+		}
+
+		// fmt.Println(date)
+		monthView(cal, date, date.AddDate(0, 2, 0), compare)
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -37,41 +63,65 @@ func Execute() {
 }
 
 func init() {
-	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports persistent flags, which, if defined here,
-	// will be global for your application.
-
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.cal8.yaml)")
-
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// Flags which apply to every command.
+	rootCmd.PersistentFlags().StringP("start-date", "s", "2020-07-06", "day to start the calendar, YYYY-MM-DD format")
+	rootCmd.PersistentFlags().StringArrayP("days", "d", cal8.Days, "days to use for the calendar")
+	rootCmd.PersistentFlags().BoolP("compare", "c", false, "toggles compare mode. In compare mode, the new calendar along with its variant are shown side by side")
 }
 
-// initConfig reads in config file and ENV variables if set.
-func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// Search config in home directory with name ".cal8" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".cal8")
+// getCal parses cmd's flags to return a cal8.Calendar struct. If it encounters an error, it will exit instead of
+// returning it.
+func getCal(cmd *cobra.Command) cal8.Calendar {
+	startDateRaw, err := cmd.PersistentFlags().GetString("start-date")
+	if err != nil {
+		fmt.Println("error parsing start date flag:", err)
+		os.Exit(1)
 	}
 
-	viper.AutomaticEnv() // read in environment variables that match
+	startDate, err := time.Parse("2006-01-02", startDateRaw)
+	if err != nil {
+		fmt.Println("error parsing start date flag:", err)
+		os.Exit(1)
+	}
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
+	days, err := cmd.PersistentFlags().GetStringArray("days")
+	if err != nil {
+		fmt.Println("error parsing days flag:", err)
+		os.Exit(1)
+	}
+
+	return cal8.NewCalendar(days, startDate.Year(), int(startDate.Month()), startDate.Day())
+}
+
+// monthView shows the calendar for the months specified.
+func monthView(cal cal8.Calendar, start, end time.Time, compare bool) {
+	if compare {
+		// show side by side comparison on modified calendar and normal.
+		for curr := start; !(curr.Year() == end.Year() && curr.Month() > end.Month()); curr = curr.Local().AddDate(0, 1, 0) {
+			modified := cal.StringMonth(curr.Year(), int(curr.Month()))
+			normal := cal8.GetNormalDay.StringMonth(curr.Year(), int(curr.Month()))
+
+			fmt.Println(cal8.HorizontalAppend(modified, normal, " |  "))
+			fmt.Println("")
+		}
+
+		return
+	}
+
+	// print modified calendar in groups of two
+	for curr := start; !(curr.Year() == end.Year() && curr.Month() > end.Month()); curr = curr.Local().AddDate(0, 2, 0) {
+		currNext := curr.AddDate(0, 1, 0)
+		m1 := cal.StringMonth(curr.Year(), int(curr.Month()))
+
+		if currNext.Year() == end.Year() && currNext.Month() > end.Month() {
+			fmt.Println(m1)
+			fmt.Println("")
+			break
+		}
+
+		m2 := cal.StringMonth(currNext.Year(), int(currNext.Month()))
+
+		fmt.Println(cal8.HorizontalAppend(m1, m2, "   "))
+		fmt.Println("")
 	}
 }
--- main.go
+++ main.go
@@ -1,13 +1,9 @@
 package main
 
-import (
-	"fmt"
-
-	"github.com/ollybritton/cal8/pkg/cal8"
-)
+import "github.com/ollybritton/cal8/cmd"
 
 func main() {
-	// cmd.Execute()
+	cmd.Execute()
 
 	// a := cal8.HorizontalAppend("hello\nhow\nare", "hi\nam\ngood", "   ")
 	// b := cal8.HorizontalAppend("what\nis\nup\ndude", "I\nhave\ncrippling\ndepression", "   ")
@@ -17,13 +13,13 @@ func main() {
 	// fmt.Println(b)
 	// fmt.Println("")
 	// fmt.Println(cal8.HorizontalAppend(a, b, "   "))
-	cal := cal8.NewCalendar(cal8.Days, 2020, 7, 6)
-	for i := 1; i <= 12; i++ {
-		a := cal.StringMonth(2020, i)
-		b := cal8.GetNormalDay.StringMonth(2020, i)
-		fmt.Println(cal8.HorizontalAppend(a, b, "   "))
-		fmt.Print("\n\n")
-	}
+	// cal := cal8.NewCalendar(cal8.Days, 2020, 7, 6)
+	// for i := 1; i <= 12; i++ {
+	// 	a := cal.StringMonth(2020, i)
+	// 	b := cal8.GetNormalDay.StringMonth(2020, i)
+	// 	fmt.Println(cal8.HorizontalAppend(a, b, "   "))
+	// 	fmt.Print("\n\n")
+	// }
 
 	// fmt.Println(cal8.GetDay.Query(2020, 2, 1))
 
